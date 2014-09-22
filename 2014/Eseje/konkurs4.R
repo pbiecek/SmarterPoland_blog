b597a9b09ee5d1855b410d51b776aef91cf677a2
pbiecek <przemyslaw.biecek@gmail.com>
2014-09-22 04:54:42 +0200

    Ilustracje dla archivist

--- 2014/Eseje/konkurs4.R
+++ 2014/Eseje/konkurs4.R
@@ -11,3 +11,16 @@ ggvis(df, ~przyczyny, ~liczba.odpowiedzi) %>%
     grid = list(stroke = "white")
   ))
 
+
+
+x <- c('ggplot2', 'ggplot2 + tufte', 'Excel', 'Tableau', 'plotly')
+y <- c(183, 22, 28, 29, 8)
+
+df <- data.frame(zrodlo = factor(x, levels=x), liczba.odpowiedzi = y, stringsAsFactors = FALSE)
+
+ggvis(df, ~zrodlo, ~liczba.odpowiedzi) %>%
+  layer_bars(width = 0.75) %>%
+  add_axis("x", properties = axis_props(
+    grid = list(stroke = "white")
+  ))
+
