12deb51137adf113ccd9ffec69bd64bd99d55ad7
pbiecek <przemyslaw.biecek@gmail.com>
2014-10-26 15:46:01 +0100

    Dane do wpisu o Banku Danych Lokalnych

---
+++ 2014/BankDanychLokalnych/dostepDoBDL.R
@@ -0,0 +1,20 @@
+library(SmarterPoland)
+
+getBDLsearch("czas")
+
+(pracujacy <- getBDLseries(metric_id = "744"))
+pracujacy <- pracujacy %>%
+  mutate(value = as.numeric(value)) %>%
+  filter(dimension.1 != "ogółem")
+
+pl <- ggplot(pracujacy, aes(x=year, y=value, color=dimension.1)) +
+  geom_point(size=3) +  geom_line() + 
+  facet_wrap(~dimension, scales="free_y") +
+  ggtitle("Pracujący wg wymiaru czasu pracy i płci") +
+  ylab("w tys. osób") + xlab("rok")
+
+ggsave(pl, width=10, height = 7, file="bdl.png")
+
+
+
+getBDLsearch("dochody")
