06ec847af391816edf5f5e89b0069bd6b936a0db
pbiecek <przemyslaw.biecek@gmail.com>
2014-11-13 16:55:21 +0100

    Uaktualniony wykres

--- dane/WyjazdyPoslow/wyjazdyPoslow.R
+++ dane/WyjazdyPoslow/wyjazdyPoslow.R
@@ -5,7 +5,7 @@ load("df.rda")
 df2 <- df %>% 
   arrange(-kwota) %>%
   group_by(posel = paste(imie, partia)) %>% 
-  mutate(skumulowana = cumsum(kwota),
+  mutate(skumulowana = cumsum(kwota)/0.8358,
          liczba = seq_along(kwota))
 
 df3 <- df2 %>% arrange(-skumulowana) %>%
@@ -16,9 +16,9 @@ df3 <- df2 %>% arrange(-skumulowana) %>%
 
 ggplot(df2, aes(x=liczba, y=skumulowana, group=posel)) +
   geom_line() + geom_point() + 
- # geom_hline(yintercept=c(40000,81000)) +
+  geom_hline(yintercept=c(40000,81000)) +
   geom_text(data=df3, aes(x=ml, y=ms, label=posel),
             vjust=0, hjust=-0.1) +
   theme_bw() + ggtitle("Wyjazdy samochodowe posłów VII kadencji") +
-  xlab("Liczba przejazdów") + xlim(0,38) + ylab("")
+  xlab("Liczba przejazdów") + xlim(0,38) + ylab("Zadeklarowanych km")
  
