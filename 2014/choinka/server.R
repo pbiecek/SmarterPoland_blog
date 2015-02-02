a52b381d9e5c6cdee3e5f03e370adc2b86cdfede
pbiecek <przemyslaw.biecek@gmail.com>
2015-02-03 00:12:32 +0100

    nowa lepsza choinka

--- 2014/choinka/server.R
+++ 2014/choinka/server.R
@@ -1,21 +1,14 @@
 ## server.r
 library(ggplot2)
 library(scales)
-library(dplyr)
 library(Cairo)
 library(ggthemes)
 library(shiny)
-library(lubridate)
 
 load("dzch.rda")
 
-shinyServer(function(input, output, session) {
-  output$gmina <- renderImage({
-    outfile <- tempfile(fileext='.svg')
-  CairoSVG(outfile, width=12, height=12)
-
-if (input$msc == "") {
-  pl <- ggplot(chlopcy, aes(x=-0.04+col*(2-row), y=row^1, label=imie, angle=-(1.5-col)*45*(row+0.1)^.2, size=liczba, color=liczba)) + 
+zielonaChoinka <- function() {
+  ggplot(chlopcy, aes(x=-0.04+col*(2-row), y=row^1, label=imie, angle=-(1.5-col)*45*(row+0.1)^.2, size=liczba, color=liczba)) + 
     geom_text(hjust=0, vjust=0) +
     geom_text(data=dziewczynki, aes(x=0.04-col*(2-row), angle=(1.5-col)*45*(row+0.1)^.2), hjust=1, vjust=1) +
     scale_size_continuous(range=c(4,6))+
@@ -24,40 +17,29 @@ if (input$msc == "") {
     theme(legend.position="none", 
           axis.ticks=element_line(color="white", size=0),
           axis.text=element_text(color="white", size=0))
-} else {
-  chlopcy2 <- chlopcy[!grepl(tolower(chlopcy$imie), pattern= tolower(input$msc)),,drop=FALSE]
-  dziewczynki2 <- dziewczynki[!grepl(tolower(dziewczynki$imie), pattern= tolower(input$msc)),,drop=FALSE]
-  chlopcy3 <- chlopcy[grepl(tolower(chlopcy$imie), pattern= tolower(input$msc)),,drop=FALSE]
-  dziewczynki3 <- dziewczynki[grepl(tolower(dziewczynki$imie), pattern= tolower(input$msc)),,drop=FALSE]
-  
-  pl <- ggplot(chlopcy2, aes(x=-0.04+col*(2-row), y=row^1, label=imie, angle=-(1.5-col)*45*(row+0.1)^.2, size=liczba, color=liczba)) + 
-    geom_text(hjust=0, vjust=0) +
-    geom_text(data=dziewczynki2, aes(x=0.04-col*(2-row), angle=(1.5-col)*45*(row+0.1)^.2), hjust=1, vjust=1) +
-    scale_size_continuous(range=c(4,6))+
-    scale_color_gradient(low="green4",high = "green1") + 
-    theme_tufte() + ylim(-0.05,0.95) + xlim(-2.1,2.1) + xlab("") + ylab("") +
-    theme(legend.position="none", 
-          axis.ticks=element_line(color="white", size=0),
-          axis.text=element_text(color="white", size=0))
-
-  if (nrow(chlopcy3) > 0) {
-    chlopcy3$imie <- paste0(chlopcy3$imie, " (",chlopcy3$liczba,")")
-    pl <- pl + geom_text(data=chlopcy3, size=5, color="red3", hjust=0, vjust=0)
-  }
-  if (nrow(dziewczynki3) > 0) {
-    dziewczynki3$imie <- paste0(dziewczynki3$imie, " (",dziewczynki3$liczba,")")
-    pl <- pl + geom_text(data=dziewczynki3, size=5, color="red3", aes(x=0.04-col*(2-row), angle=(1.5-col)*45*(row+0.1)^.2), hjust=1, vjust=1) 
-  }
-    
 }
-print(pl)
 
-    dev.off()
+shinyServer(function(input, output, session) {
+  output$choinka <- renderImage({
+  outfile <- tempfile(fileext='.svg')
+  CairoSVG(outfile, width=12, height=12)
+
+  pl <- zielonaChoinka()
+  if (input$imie != "")  {
+    chlopcy3 <- chlopcy[grepl(tolower(chlopcy$imie), pattern= tolower(input$imie)),,drop=FALSE]
+    if (nrow(chlopcy3) > 0) 
+      pl <- pl + geom_text(data=chlopcy3, color="red3", hjust=0, vjust=0)
+    
+    dziewczynki3 <- dziewczynki[grepl(tolower(dziewczynki$imie), pattern= tolower(input$imie)),,drop=FALSE]
+    if (nrow(dziewczynki3) > 0) 
+      pl <- pl + geom_text(data=dziewczynki3, color="red3", aes(x=0.04-col*(2-row), angle=(1.5-col)*45*(row+0.1)^.2), hjust=1, vjust=1) 
       
-    # Return a list containing the filename
-    list(src = outfile,
-         width = 720,
-         height = 720)
+  }
+
+  print(pl)
+  dev.off()
+      
+  list(src = outfile, width = 720, height = 720)
     })
 })
 
--- 2014/choinka/ui.R
+++ 2014/choinka/ui.R
@@ -2,9 +2,9 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  textInput(inputId = "msc",
-                     label = "Zaznacz imiona",
-                     value = ""),
-  plotOutput("gmina", width=720, height=720)
-  )
+  textInput(inputId = "imie",
+            label = "Zaznacz imiona",
+            value = ""),
+  plotOutput("choinka", width=720, height=720)
+)
 )
