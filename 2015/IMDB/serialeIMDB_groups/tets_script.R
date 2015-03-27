09d9effae38d3e80119dbf115e6b09c1194cb89c
pbiecek <przemyslaw.biecek@gmail.com>
2015-03-27 09:30:12 +0100

    fix

--- 2015/IMDB/serialeIMDB_groups/tets_script.R
+++ 2015/IMDB/serialeIMDB_groups/tets_script.R
@@ -46,36 +46,45 @@ gr <- cut(xx - yy, c(-10,-0.5,0.5,10))
 df <- data.frame(x = xx, y = yy, gr = gr,
                name = votesGroup[,"serialName"])
     
-  etykieta <- function(data){
-    if(is.null(data)) return(NULL)
-    
-    paste0("<b>",data$name, "</b><br/> ",data$xl, ": ", data$x, " <br/>", data$yl, ": ", data$y,"")
-  }
+library(ggvis)
+
+df %.% ggvis(x =~ x, y =~ y) %.%
+  layer_points(fill = ~gr, size.hover := 200,
+               fillOpacity := 0.9,
+               fillOpacity.hover := 0.95) %.%
+  add_axis("x", title = group1) %.%
+  add_axis("y", title = group2)
+
+
+etykieta <- function(data){
+  if(is.null(data)) return(NULL)
+  paste0("<b>",data$name, "</b><br/> ", data$x, " <br/>", data$y,"")
+}
   
-  abline_data <- function (domain, intercept, slope) {
-    data.frame(x = domain, y = domain * slope + intercept)
-  }
-  layer_abline <- function (.vis, domain, intercept = 0, slope = 1, dash = 6, ...) {
-    df <- abline_data(domain, intercept, slope)
-    names(df) <- with(.vis$cur_props, c(x.update$value, y.update$value))
-    layer_paths(.vis, data = df, ..., strokeDash := dash)
-  }
+
+
+abline_data <- function (domain, intercept, slope) {
+  data.frame(x = domain, y = domain * slope + intercept)
+}
+layer_abline <- function (.vis, domain, intercept = 0, slope = 1, dash = 6, ...) {
+  df <- abline_data(domain, intercept, slope)
+  names(df) <- with(.vis$cur_props, c(x.update$value, y.update$value))
+  layer_paths(.vis, data = df, ..., strokeDash := dash)
+}
   
   
-  xy %>% 
-    ggvis(x =~ x, y =~ y) %>%
-    group_by(name, xl, yl) %>%
-    layer_points(fill = ~gr, size.hover := 200,
-                 fillOpacity := 0.9,
-                 fillOpacity.hover := 0.95) %>%
-    layer_abline(c(7,10)) %>%
-    add_axis("x", title = "Criterion for OX axis") %>%
-    add_axis("y", title = "Criterion for OY axis") %>%
-    add_tooltip(etykieta, "hover") %>%
-    set_options(width = 640,padding = padding(10, 10, 50, 50)) %>%
-    bind_shiny("serialPlot")
-  
-})
+df %>% 
+  ggvis(x =~ x, y =~ y) %>%
+  group_by(name) %>%
+  layer_points(fill = ~gr, size.hover := 200,
+               fillOpacity := 0.9,
+               fillOpacity.hover := 0.95) %>%
+  layer_abline(c(7,10)) %>%
+  add_axis("x", title = group1) %>%
+  add_axis("y", title = group2) %>%
+  hide_legend(scales = "fill") %>%
+  add_tooltip(etykieta, "hover") 
+
 
 
 
